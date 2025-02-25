b3076ec713d1984376719dd6dcdccfb12c76d4e4
janmenjaya55 <janmenjayabehera@gmail.com>
2025-02-25 10:24:26 +0530

    Started commit

---
+++ src/main/java/com/gov/eygds/YgdsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.gov.eygds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgdsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgdsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gov/eygds/YgdsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.eygds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgdsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
